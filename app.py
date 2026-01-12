38fdb5e70d45c71585ececd25744d227f38b43bd
jasman <jasman0603@gmail.com>
2026-01-12 14:15:14 +0700

    add shortcut instruction

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ pages = {
 
 pg = st.navigation(pages)
 pg.run()
+
+#create streamtlit shortcut
+# cmd.exe /k "F:\python_project\.venv\Scripts\python.exe -m streamlit run F:\python_project\Stremlit_project_dashboard\app.py --server.headless true"
